97a1257c39ac7ec1b3e96a4c9b1e8a38ef715302
cristianducu <cristian.ducu@icloud.com>
2023-10-14 21:37:23 +0300

    getUser is kind of working

--- src/nostr-tools/getUser.ts
+++ src/nostr-tools/getUser.ts
@@ -1,33 +1,40 @@
 import {SimplePool} from 'nostr-tools';
-import {IUser} from '../utils/types'; // Replace IUser with your user data type definition
+import {IUser} from '../utils/types'; 
 import {RELAYS_URL} from '../nostr-tools/nostrRelays';
 import {DEFAULT_TAG, USER_TAG} from './nostrTags';
-import {generatePublic} from './generateKeys'; // Replace with your actual utility function
+import {generatePublic} from './generateKeys'; 
 
 const pool = new SimplePool();
 
-export const getUser = (privateKey: string): Promise<IUser> => {
+export const getUser = (privateKey: string): Promise<IUser[]> => {
   return new Promise((resolve, reject) => {
-    let fetchedUser: IUser = {} as IUser;
+    let fetchedUser: IUser[] = [];
 
     // Derive the public key from the private key
     const publicKey = generatePublic(privateKey);
 
-    let userSub = pool.sub(RELAYS_URL, [{'#t': ['open-rooms', 'user']}]);
+    let userSub = pool.sub(RELAYS_URL, [
+      {
+        authors: [publicKey],
+        ...DEFAULT_TAG,
+        ...USER_TAG,
+      },
+    ]);
 
     userSub.on('event', (event: any) => {
       console.log('getUser - Event received:', event);
       const userContent: IUser = JSON.parse(event.content);
       userContent.id = event.id;
+      fetchedUser.push(userContent);
       // added resolve here because .on('end') was not being called
       resolve(fetchedUser);
-      fetchedUser = userContent;
     });
 
     (userSub as any).on('end', () => {
-      console.log('Entered .on(end) block');
+      console.log('End is working');
       resolve(fetchedUser);
     });
+
     (userSub as any).on('error', (err: any) => {
       console.log('An error occurred:', err);
       reject(err);
